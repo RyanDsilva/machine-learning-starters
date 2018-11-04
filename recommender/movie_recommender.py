24c509423394c6c022227244121bfc83f8a84e29
Ryan <ryan.dsilva.98@gmail.com>
2018-11-04 14:44:12 +0530

    Recommender

---
+++ recommender/movie_recommender.py
@@ -0,0 +1,26 @@
+import numpy as np
+from lightfm.datasets import fetch_movielens
+from lightfm import LightFM
+
+data = fetch_movielens(min_rating=4.0)
+
+model = LightFM(loss='warp')
+model.fit(data['train'], epochs=30, num_threads=2)
+
+n_users, n_items = data['train'].shape
+
+
+def sample_recommendation(model, data, user_id):
+    known_positives = data['item_labels'][data['train'].tocsr()[
+        user_id].indices]
+    scores = model.predict(user_id, np.arange(n_items))
+    top_items = data['item_labels'][np.argsort(-scores)]
+    print("Liked Already:")
+    for x in known_positives[:3]:
+        print(x)
+    print("Recommended:")
+    for x in top_items[:3]:
+        print(x)
+
+
+print(sample_recommendation(model, data, 100))
