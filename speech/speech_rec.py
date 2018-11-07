f6f5dd44c2c1d8532f9d1d96d42eb49e339f7890
Ryan <ryan.dsilva.98@gmail.com>
2018-11-07 14:13:35 +0530

    Speech

---
+++ speech/speech_rec.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+import KEYS
+
+AUDIO_FILE = "./test.flac"
+
+
+rec = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Speak into th microphone now...")
+    audio = rec.listen(source)
+
+#with sr.AudioFile(AUDIO_FILE) as source:
+#  audio = rec.record(source)
+
+try:
+    print(rec.recognize_wit(audio, key=KEYS.WIT_AI_KEY))
+except sr.UnknownValueError:
+    print("Could not understand audio!")
+except sr.RequestError as e:
+    print("Could Not connect to server!; {0}".format(e))
